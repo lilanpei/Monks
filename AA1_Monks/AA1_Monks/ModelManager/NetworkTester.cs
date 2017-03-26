1db329a22af006bd585eefbe9561ad344d812495
lilanpei <lilanpei@gmail.com>
2017-03-26 23:29:25 +0200

    ROC polt

--- AA1_Monks/AA1_Monks/ModelManager/NetworkTester.cs
+++ AA1_Monks/AA1_Monks/ModelManager/NetworkTester.cs
@@ -9,6 +9,77 @@ namespace AA1_Monks.ModelManager
 {
     class NetworkTester
     {
+        public static void PlotROC(string testSetLocation, BasicNetwork network, string errorLogLocation, double threshold)
+        {
+            double TP = 0, FP = 0, TN = 0, FN = 0;
+            double actualyes = 0;
+            double actualNo = 0;
+            double predictedYes = 0;
+            double predictedNo = 0;
+            var testingSet = DataManager.DataLoader.LoadMonksData(testSetLocation);
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(errorLogLocation,true))
+            {
+
+                foreach (var pair in testingSet)
+                {
+                    var o = network.Compute(pair.Input);
+
+
+                    if ((int)pair.Ideal[0] == 1)
+                    {
+                        actualyes++;
+                    }
+                    else
+                    {
+                        actualNo++;
+                    }
+
+                    if ((o[0] >= threshold ? 1 : 0) == 1)
+                    {
+                        predictedYes++;
+                    }
+                    else predictedNo--;
+
+                    if ((o[0] >= threshold ? 1 : 0) == 1 && (int)pair.Ideal[0] == 1)
+                    {
+                        TP++;
+                    }
+                    else if ((o[0] >= threshold ? 1 : 0) == 0 && (int)pair.Ideal[0] == 0)
+                    {
+                        TN++;
+                    }
+
+                    else if ((o[0] >= threshold ? 1 : 0) == 1 && (int)pair.Ideal[0] == 0)
+                    {
+                        FP++;
+                    }
+                    else if ((o[0] >= threshold ? 1 : 0) == 0 && (int)pair.Ideal[0] == 1)
+                    {
+                        FN++;
+                    }
+                    //file.WriteLine("Actual=" + (o[0] >= threshold ? 1 : 0) + ", Ideal=" + pair.Ideal[0]);
+
+                }
+                //file.WriteLine("Accuracy:" + (TP + TN) / testingSet.Count);
+                //file.WriteLine("Misclassification Rate:" + (FP + FN) / testingSet.Count);
+                //file.WriteLine("True Positive Rate(Recall):" + TP / actualyes);
+                //file.WriteLine("False Positive Rate:" + FP / actualNo);
+                file.WriteLine(1 - TN / actualNo + "," + TP / actualyes);
+                //file.WriteLine("Precision:" + TP / predictedYes);
+                //file.WriteLine("Prevalence:" + actualyes / testingSet.Count);
+
+                //file.WriteLine("predicted yes:" + predictedYes);
+                //file.WriteLine("predicted np:" + predictedNo);
+
+                //file.WriteLine("Consusion Matrix:");
+
+                //file.WriteLine("True Positive:" + TP);
+                //file.WriteLine("True Negative:" + TN);
+                //file.WriteLine("False Positive:" + FP);
+                //file.WriteLine("False Negative:" + FN);
+            }
+        }
         public static void TestNetwork(string testSetLocation, BasicNetwork network, string errorLogLocation)
         {
             double TP = 0, FP = 0, TN = 0, FN = 0;
@@ -16,6 +87,7 @@ namespace AA1_Monks.ModelManager
             double actualNo = 0;
             double predictedYes = 0;
             double predictedNo = 0;
+            double threshold = 0.5;
             var testingSet = DataManager.DataLoader.LoadMonksData(testSetLocation);
             using (System.IO.StreamWriter file =
                 new System.IO.StreamWriter(errorLogLocation))
@@ -35,30 +107,30 @@ namespace AA1_Monks.ModelManager
                         actualNo++;
                     }
 
-                    if ((o[0] >= 0.5 ? 1 : 0) == 1)
+                    if ((o[0] >= threshold ? 1 : 0) == 1)
                     {
                         predictedYes++;
                     }
                     else predictedNo--;
 
-                    if ((o[0] >= 0.5 ? 1 : 0) == 1 && (int)pair.Ideal[0] == 1)
+                    if ((o[0] >= threshold ? 1 : 0) == 1 && (int)pair.Ideal[0] == 1)
                     {
                         TP++;
                     }
-                    else if ((o[0] >= 0.5 ? 1 : 0) == 0 && (int)pair.Ideal[0] == 0)
+                    else if ((o[0] >= threshold ? 1 : 0) == 0 && (int)pair.Ideal[0] == 0)
                     {
                         TN++;
                     }
 
-                    else if ((o[0] >= 0.5 ? 1 : 0) == 1 && (int)pair.Ideal[0] == 0)
+                    else if ((o[0] >= threshold ? 1 : 0) == 1 && (int)pair.Ideal[0] == 0)
                     {
                         FP++;
                     }
-                    else if ((o[0] >= 0.5 ? 1 : 0) == 0 && (int)pair.Ideal[0] == 1)
+                    else if ((o[0] >= threshold ? 1 : 0) == 0 && (int)pair.Ideal[0] == 1)
                     {
                         FN++;
                     }
-                    file.WriteLine("Actual=" + (o[0] >= 0.5 ? 1 : 0) + ", Ideal=" + pair.Ideal[0]);
+                    file.WriteLine("Actual=" + (o[0] >= threshold ? 1 : 0) + ", Ideal=" + pair.Ideal[0]);
 
                 }
                 file.WriteLine("Accuracy:" + (TP + TN) / testingSet.Count);
@@ -72,7 +144,7 @@ namespace AA1_Monks.ModelManager
                 file.WriteLine("predicted yes:" + predictedYes);
                 file.WriteLine("predicted np:" + predictedNo);
 
-               file.WriteLine("Consusion Matrix:");
+                file.WriteLine("Consusion Matrix:");
 
                 file.WriteLine("True Positive:" + TP);
                 file.WriteLine("True Negative:" + TN);
--- AA1_Monks/AA1_Monks/Program.cs
+++ AA1_Monks/AA1_Monks/Program.cs
@@ -12,11 +12,13 @@ namespace AA1_Monks
             string testSetLocation = Properties.Settings.Default.testDataLocation;
 
 
-            var network = ModelManager.NeworkTrainer.TrainNetwork(trainSetLocation, @"model2.mo", @"c:\traininghsitory_" + DateTime.Now.ToString().Replace(":", "-").Replace("/", "_") + ".log", 5000, 0.001, NetworkTrainer.Quick);
-          //  var network = ModelManager.NeworkTrainer.LoadNetwork(@"C:\Users\ahmad\Source\Repos\Monks\AA1_Monks\AA1_Monks\bin\Debug\model1.mo");
+            //var network = ModelManager.NeworkTrainer.TrainNetwork(trainSetLocation, @"model2.mo", @"c:\traininghsitory_" + DateTime.Now.ToString().Replace(":", "-").Replace("/", "_") + ".log", 5000, 0.001, NetworkTrainer.Quick);
+            var network = ModelManager.NeworkTrainer.LoadNetwork(@"E:\Dropbox\Master Course\SEM-3\ML\Monk\AA1_Monks\AA1_Monks\bin\Debug\model2.mo");
 
-            ModelManager.NetworkTester.TestNetwork(testSetLocation, network,@"C:\out.log");
+            for (double i = 0; i <=  1; i += 0.05)
+            {
+                ModelManager.NetworkTester.PlotROC(testSetLocation, network, @"ROC.log", i);
+            }           
         }
-
     }
 }
