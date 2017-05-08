d6b1d76cfc21395b61015c402872e5d23eca7135
alleboudy <ahmad.alleboudy@outlook.com>
2017-05-08 22:12:14 +0200

    added relu

---
+++ AA1_Monks/AA1_MLP/Activations/ActivationRelu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA1_MLP.Activations
+{
+    [Serializable]
+    class ActivationLeakyRelu : IActivation
+    {
+        public MathNet.Numerics.LinearAlgebra.Vector<double> CalculateActivation(MathNet.Numerics.LinearAlgebra.Vector<double> x)
+        {
+            return x.Map(s => s > 0 ? s : 0.01 * s);
+        }
+
+        public MathNet.Numerics.LinearAlgebra.Vector<double> CalculateDerivative(MathNet.Numerics.LinearAlgebra.Vector<double> x)
+        {
+            return x.Map(s => s > 0 ? 1 : 0.01 );
+
+        }
+    }
+}
