8cc79554a06042ff240c58af4e65e744fbb419cf
alleboudy <ahmad.alleboudy@outlook.com>
2017-05-20 23:03:03 +0200

    grid search for parameters

--- AA1_Monks/AA1_CUP/Program.cs
+++ AA1_Monks/AA1_CUP/Program.cs
@@ -16,17 +16,8 @@ namespace AA1_CUP
         static void Main(string[] args)
         {
             // Building a simple network
-            /*     Network n = new Network(new List<Layer>() {
-                     new Layer(new ActivationIdentity(),true,10),
-                     new Layer(new ActivationLeakyRelu(),true,20),
-                  //   new Layer(new ActivationLeakyRelu(),true,40),
 
 
-                     new Layer(new ActivationLeakyRelu(),false,2),
-                     }, new MathNet.Numerics.Distributions.Normal(0, 1), true, false);
-
-
-                 */
             CupDataManager dm = new CupDataManager();
             DataSet wholeSet = dm.LoadData(Properties.Settings.Default.TrainingSetLocation, 10, 2);
             int trainSplit = (int)(0.6 * wholeSet.Inputs.RowCount);
@@ -40,43 +31,100 @@ namespace AA1_CUP
               inputs: wholeSet.Inputs.SubMatrix(trainSplit, (wholeSet.Inputs.RowCount - trainSplit) / 2, 0, wholeSet.Inputs.ColumnCount),
          labels: wholeSet.Labels.SubMatrix(trainSplit, (wholeSet.Inputs.RowCount - trainSplit) / 2, 0, wholeSet.Labels.ColumnCount));
 
-            /*
-
-                BackPropagation bp = new BackPropagation();
-
-                var learningCurve = bp.Train(n,
-                         trainingSplit,
-                         0.000001,
-                         500000,
-                         true,
-                         regularizationRate: 0.001,
-                         regularization: AA1_MLP.Enums.Regularizations.L2,
-                         momentum: 0.9,
-                         validationSet: ValidationSplit,
-
-                         MEE: true,
-                        resilient: true, resilientUpdateAccelerationRate: 10, resilientUpdateSlowDownRate: 1,
-                        reduceLearningRate: true,
-                        learningRateReduction: 0.8,
-                        numberOfReductions: 3,
-                        learningRateReductionAfterEpochs:7500
-                         );
-
-                //writing the learning curve data to desk (ugly for memory, but simple)
-                File.WriteAllText(Properties.Settings.Default.LearningCurveLocation, string.Join("\n", learningCurve.Select(s => string.Join(",", s))));
-                */
-            //    AA1_MLP.Utilities.ModelManager.SaveNetowrk(n, "model.AA1");
-            var n = AA1_MLP.Utilities.ModelManager.LoadNetwork("model.AA1");
             DataSet TestSplit = new DataSet(
-            inputs: wholeSet.Inputs.SubMatrix(trainSplit + (wholeSet.Inputs.RowCount - trainSplit) / 2, (int)Math.Ceiling((double)(wholeSet.Inputs.RowCount - trainSplit) / 2), 0, wholeSet.Inputs.ColumnCount),
-            labels: wholeSet.Labels.SubMatrix(trainSplit + (wholeSet.Inputs.RowCount - trainSplit) / 2, (int)Math.Ceiling((double)(wholeSet.Inputs.RowCount - trainSplit) / 2), 0, wholeSet.Labels.ColumnCount));
-            double MEE = 0;
-            var log = ModelManager.TesterRegression(TestSplit, n, out MEE);
+       inputs: wholeSet.Inputs.SubMatrix(trainSplit + (wholeSet.Inputs.RowCount - trainSplit) / 2, (int)Math.Ceiling((double)(wholeSet.Inputs.RowCount - trainSplit) / 2), 0, wholeSet.Inputs.ColumnCount),
+       labels: wholeSet.Labels.SubMatrix(trainSplit + (wholeSet.Inputs.RowCount - trainSplit) / 2, (int)Math.Ceiling((double)(wholeSet.Inputs.RowCount - trainSplit) / 2), 0, wholeSet.Labels.ColumnCount));
+
+            BackPropagation bp = new BackPropagation();
 
 
-            File.WriteAllText("scatter.txt", string.Join("\n", log.Select(s => string.Join(",", s))));
+            List<int> PossibleHiddenUnits = new List<int>();
+            for (int numberOfUnits = 2; numberOfUnits < 100; numberOfUnits += 8)
+            {
+                PossibleHiddenUnits.Add(numberOfUnits);
+            }
+            List<double> Regularizations = new List<double>() { 1, 0.1, 0.01, 0.001, 0.0001, 0.00001 };
 
-            Console.WriteLine(MEE);
+            List<double> Momentums = new List<double>() { 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 };
+            List<double> learningRate = new List<double>() { 1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.000001 };
+
+            Directory.CreateDirectory("learningCurves");
+            Directory.CreateDirectory("scatters");
+
+            foreach (var hidn in PossibleHiddenUnits)
+            {
+                foreach (var reg in Regularizations)
+                {
+                    foreach (var mo in Momentums)
+                    {
+                        foreach (var lr in learningRate)
+                        {
+
+                            string pre = string.Format("hidn{0}_reg{1}_mo{2}_lr{3}", hidn, reg, mo, lr);
+
+                            Network n = new Network(new List<Layer>() {
+                     new Layer(new ActivationIdentity(),true,10),
+                     new Layer(new ActivationLeakyRelu(),true,hidn),
+                  //   new Layer(new ActivationLeakyRelu(),true,40),
+
+
+                     new Layer(new ActivationLeakyRelu(),false,2),
+                     }, new MathNet.Numerics.Distributions.Normal(0, 1), true, false);
+
+                            try
+                            {
+
+
+                                var learningCurve = bp.Train(n,
+                                         trainingSplit,
+                                         lr,
+                                         500000,
+                                         true,
+                                         regularizationRate: reg,
+                                         regularization: AA1_MLP.Enums.Regularizations.L2,
+                                         momentum: mo,
+                                         validationSet: ValidationSplit,
+
+                                         MEE: true
+                                    /*  resilient: true, resilientUpdateAccelerationRate: 10, resilientUpdateSlowDownRate: 1,
+                                      reduceLearningRate: true,
+                                      learningRateReduction: 0.8,
+                                      numberOfReductions: 3,
+                                      learningRateReductionAfterEpochs: 7500*/
+                                         );
+
+                                //writing the learning curve data to desk (ugly for memory, but simple)
+                                File.WriteAllText("learningCurves/" + pre + "learningCurve.txt", string.Join("\n", learningCurve.Select(s => string.Join(",", s))));
+
+                                //    AA1_MLP.Utilities.ModelManager.SaveNetowrk(n, "model.AA1");
+                                // var n = AA1_MLP.Utilities.ModelManager.LoadNetwork("model.AA1");
+
+                                double MEE = 0;
+                                var log = ModelManager.TesterRegression(TestSplit, n, out MEE);
+
+
+                                File.WriteAllText("scatters/" + pre + "scatter.txt", string.Join("\n", log.Select(s => string.Join(",", s))));
+
+                                File.AppendAllText("MEEs.txt", pre + ":" + MEE);
+
+                                Console.WriteLine(MEE);
+
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine(pre + " Failed!");
+                                File.AppendAllText("fails.txt", pre);
+
+
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+
 
         }
     }
